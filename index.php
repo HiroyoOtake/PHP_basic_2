76a428547824386ed1047ef1fb4124a2b293dde7
otake <otake.hiroyo@gmail.com>
2016-11-30 12:47:48 +0900

    組み込み関数

--- index.php
+++ index.php
@@ -1,30 +1,19 @@
 <?php
 
-// 現在時刻によってあいさつを変える関数 greet() を作成せよ
-// (i)   現在時刻が6時以降  - 12時より前 -> おはようございます
-// (ii)  現在時刻が12時以降 - 18時より前 -> こんにちは
-// (iii) 現在時刻が18時以降 -  6時より前 -> こんばんは
+// implode() explode()
+$fruits = array('りんご' , 'みかん' , 'いちご');
 
-$hour = date('H');
+$data = implode("#", $fruits);
+// var_dump($data); //string(29)"りんご#みかん#いちご"
+$test = explode('#', $data);
+// var_dump($test); //array(3) { [0]=> string(9) "りんご" [1]=> string(9) "みかん" [2]=> string(9) "いちご"  }
 
-function great($hour,$to = '名無し' )
-{
-	echo '現在時刻は' . $hour . '時ですね。' . $to . 'さん';
-	if ($hour >= 6 && $hour < 12)
-	{
-		echo 'おはようございます';
-	}
-	elseif ($hour >= 12 && $hour < 18)
-	{
-		echo 'こんにちは';
-	}
-	else 
-	{
-		echo 'こんばんは';
-	}
-}
+//list()
+list($first, $second, $third) = explode("#", $data);
+// echo $first;
 
-great($hour, '田中');
-
+//data()
+// Y年 m月 d日 H時 i分 s秒
+echo date('Y-m-d H:i:s');
 
 ?>
