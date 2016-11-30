eb367fd8abfb1eada04fea562a805f407f637584
otake <otake.hiroyo@gmail.com>
2016-11-30 13:14:42 +0900

    ファイルの読み込み

--- index.php
+++ index.php
@@ -1,19 +1,19 @@
 <?php
 
-// 1. ファイルを開く fopen()
-$fileName = "tests.dat";
-$data = "書き込みテスト";
+// 1. ファイルを開く fopen
+$fileName = "test.dat";
 
-$fp = fopen($fileName, "a");
-// モード a:追記 w:上書き r:読み取り
-
-// 2. ファイルにデータを書き込む fwrite()
-if (fwrite($fp, $data) === false)
+if (!$fp = fopen($fileName, "r"))
 {
-	echo "書き込みができませんでした。";
+	echo "ファイルを開けませんでした";
 	exit;
 }
-echo "書き込みが成功しました!";
+// 2. ファイルのデータを読み込む fread() file()
+$content = fread($fp, filesize($fileName));
+
+// var_dump($content);
+$test = file($fileName); //配列で返してくれる
+var_dump($test);
 
 // 3. ファイルを閉じる fclose()
 fclose($fp);
