e96ab8da7ff39c8d85e2dd69f24148ecc03b520b
otake <otake.hiroyo@gmail.com>
2016-11-30 12:07:23 +0900

    関数と引数と返り値

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+// Y(X) = 2X + 1
+// X = 2 -> Y = 5  // 2が引数で5が返り値
+
+$fruits = array('りんご','みかん','いちご');
+
+// 関数名(引数) -> 返り値
+count($fruits); //$fruitsが引数
+var_dump(count($fruits)); //int(3)が返り値
+
+?>
